7b9603745a6a91943c32687cec83c5b7a59c0ec8
misterdan100 <danielmca60@gmail.com>
2025-03-05 05:41:28 -0500

    fix: sequelize config connection with pg to vercel deploy

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const db = new Sequelize(process.env.DATABASE_URL)
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialectModule: pg,
+})
 
 export default db
